7c83f8017d7b7eda2ac92919aff1d4ec8f6bd4d3
askDing <741474596@qq.com>
2020-12-30 11:18:58 +0800

    create port_scan.sh

--- OS_Dectect.sh
+++ OS_Dectect.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # 这是探测系统平台的脚本
 
-if type wevtutil &> /dev/null
+if type wevtutil &> /dev/null  # Windows事件命令行实用程序
 then
   OS=MSWin
-elif type scutil &> /dev/null
+elif type scutil &> /dev/null  #macOS中管理系统配置参数命令
 then
   OS=macOS
 else
