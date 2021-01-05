29091dfcce5220199ba031ca2e97674b4245ecd1
askDing <741474596@qq.com>
2021-01-05 17:07:05 +0800

    update select_learning

---
+++ select_learn.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+echo "Which Operating System do you like?"
+
+select os in Ubuntu LinuxMint Windows8 Windows10 WindowsXP
+do
+  case $os in
+    "Ubuntu"|"LinuxMint")
+      echo "I also use $os."
+    ;;
+    "Windows8" | "Windows10" | "WindowsXP")
+      echo "Why don't you try Linux?"
+    ;;
+    *)
+      echo "Invalid entry."
+      break
+    ;;
+  esac
+done
+
