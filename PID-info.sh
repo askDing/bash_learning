233d78ff0d8ef82c119d0251ff911f1879145c8c
askDing <741474596@qq.com>
2021-01-07 14:47:57 +0800

    modified PID-info.sh

--- PID-info.sh
+++ PID-info.sh
@@ -1,12 +1,25 @@
 #! /bin/bash
+# 统计Linux进程相关数量信息
+# 请以root用户运行
+# Author askDin
+
+#使用root身份运行脚本
+if [ $UID -ne 0 ]; then
+  echo "Please run this script with root :)"
+  exit 0
+fi
 
 running=0
 sleeping=0
 stoped=0
 zombie=0
 
+# 在proc目录下所有以数字开始的都是当前计算机正在运行的进程的进程PID
+# 每个PID编号的目录下记录有该进程的相关信息
 for pid in /proc/[1-9]* ; do
-  procs=$[proces+1]
+  procs=$[pid+1]
+
+  # 每个PID目录下都有一个stat文件，该文件的第3列是该进程的状态信息
   stat=$(awk '{print $3}' $pid/stat)
   case $stat in
     R)
