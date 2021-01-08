244fedf521ad70997ea4c4b1b3e3543325bca259
askDing <741474596@qq.com>
2021-01-08 13:36:32 +0800

    tput_learn.sh

---
+++ tput_learn.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#
+# 颜色学习
+#
+c0=$(tput setaf 0)
+c1=$(tput setaf 1)
+c2=$(tput setaf 2)
+c3=$(tput setaf 3)
+c4=$(tput setaf 4)
+c5=$(tput setaf 5)
+c6=$(tput setaf 6)
+c7=$(tput setaf 7)
+RESET=$(tput sgr0)
+
+echo "${c0}color${RESET}"
+echo "${c1}color${RESET}"
+echo "${c2}color${RESET}"
+echo "${c3}color${RESET}"
+echo "${c4}color${RESET}"
+echo "${c5}color${RESET}"
+echo "${c6}color${RESET}"
+echo "${c7}color${RESET}"
